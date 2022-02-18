989690a7496d6996e90653b40c22b68cfc7d61aa
Ben Razor <chrisbenwar1@googlemail.com>
2022-02-18 14:14:32 +0000

    Fix error caused by non-json events

--- thegraph/nk/src/mapping.ts
+++ thegraph/nk/src/mapping.ts
@@ -23,132 +23,135 @@ export function handleReceipt(
     let eventStr = '';
     let l = logs[i];
 
-    let obj = json.try_fromString(l.replace('EVENT_JSON:', ''));
+    if(l.startsWith('EVENT_JSON:')) {
+      let obj = json.try_fromString(l.replace('EVENT_JSON:', ''));
 
-    if(obj.isOk) {
-      let jo = obj.value.toObject();
-      let event = jo.get('event');
-      eventStr = event ? event.toString() : '';
+      if(obj.isOk) {
+        let jo = obj.value.toObject();
+        let event = jo.get('event');
+        eventStr = event ? event.toString() : '';
 
-      log.info('ev {}', [eventStr]);
-      if(eventStr == 'nft_mint') {
-        let data = jo.get('data');
-        let dataObj = data ? data.toArray() : null;
-        let mintInfoJsonValue = dataObj ? dataObj[0] : null;
-        let mintInfo = mintInfoJsonValue ? mintInfoJsonValue.toObject() : null;
+        log.info('ev {}', [eventStr]);
+        if(eventStr == 'nft_mint') {
+          let data = jo.get('data');
+          let dataObj = data ? data.toArray() : null;
+          let mintInfoJsonValue = dataObj ? dataObj[0] : null;
+          let mintInfo = mintInfoJsonValue ? mintInfoJsonValue.toObject() : null;
 
-        if(mintInfo) {
-          let tokenIdJsonValue = mintInfo.get('token_ids');
-          let tokenId = tokenIdJsonValue ? tokenIdJsonValue.toArray()[0].toString() : '';
+          if(mintInfo) {
+            let tokenIdJsonValue = mintInfo.get('token_ids');
+            let tokenId = tokenIdJsonValue ? tokenIdJsonValue.toArray()[0].toString() : '';
 
-          if(tokenId) {
-            let entity = new NearKart(tokenId)
+            if(tokenId) {
+              let entity = new NearKart(tokenId)
 
-            entity.ownerId = homeAccount;
-            entity.tokenId = tokenId;
-            entity.save();
+              entity.ownerId = homeAccount;
+              entity.tokenId = tokenId;
+              entity.save();
+            }
           }
         }
-      }
-      else if(eventStr == 'configure_on_mint' || eventStr == 'configure_on_upgrade') {
-        let data = jo.get('data');
-        let kartMeta = data ? data.toObject() : null;
+        else if(eventStr == 'configure_on_mint' || eventStr == 'configure_on_upgrade') {
+          let data = jo.get('data');
+          let kartMeta = data ? data.toObject() : null;
 
-        if(kartMeta) {
-          let tokenIdJsonValue = kartMeta.get('token_id');
-          let tokenId = tokenIdJsonValue ? tokenIdJsonValue.toString() : '';
+          if(kartMeta) {
+            let tokenIdJsonValue = kartMeta.get('token_id');
+            let tokenId = tokenIdJsonValue ? tokenIdJsonValue.toString() : '';
 
-          if(tokenId) {
-            let nameJsonValue = kartMeta.get('name');
-            let name = nameJsonValue ? nameJsonValue.toString() : '';
+            if(tokenId) {
+              let nameJsonValue = kartMeta.get('name');
+              let name = nameJsonValue ? nameJsonValue.toString() : '';
 
-            let mediaJsonValue = kartMeta.get('media');
-            let media = mediaJsonValue ? mediaJsonValue.toString() : '';
+              let mediaJsonValue = kartMeta.get('media');
+              let media = mediaJsonValue ? mediaJsonValue.toString() : '';
 
-            let nearKart = NearKart.load(tokenId);
+              let nearKart = NearKart.load(tokenId);
+              
+              if(nearKart) {
+                nearKart.name = name;
+                nearKart.media = media;
+                nearKart.mediaHistory = nearKart.mediaHistory ? nearKart.mediaHistory as Array<string> : [media];
+                nearKart.save();
+              }
+            }
+          }
+        }
+        else if(eventStr == 'game_simple_battle') {
+          let data = jo.get('data');
+          let dataObj = data ? data.toObject() : null;
+
+          if(dataObj) {
+            let entity = new NearKartsSimpleBattle(receiptWithOutcome.receipt.id.toHex())
+
+            entity.homeAccount = homeAccount;
+            entity.timestamp = timestamp;
+
+            let homeTokenId = dataObj.get('home_token_id');
+            entity.homeTokenId = homeTokenId ? homeTokenId.toString() : '';
+
+            let awayTokenId = dataObj.get('away_token_id');
+            entity.awayTokenId = awayTokenId ? awayTokenId.toString() : '';
+
+            let winner = dataObj.get('winner');
+            entity.winner = winner ? winner.toI64() as i32 : 0;
             
+            let battle = dataObj.get('battle');
+            entity.battle = battle ? battle.toBigInt() : new BigInt(0);
+
+            let prize = dataObj.get('prize');
+            entity.prize = prize ? prize.toString() : '';
+
+            let extra = dataObj.get('extra');
+            entity.extra = extra ? extra.toString() : '';
+
+            entity.save();
+
+            let nearKart = NearKart.load(entity.homeTokenId);
+
             if(nearKart) {
-              nearKart.name = name;
-              nearKart.media = media;
-              nearKart.mediaHistory = nearKart.mediaHistory ? nearKart.mediaHistory as Array<string> : [media];
-              nearKart.save();
+              let scoreDailyId = entity.homeTokenId + '_' + period.toString();
+              let scoreDailyEntity = ScoreDaily.load(scoreDailyId);
+
+              if(!scoreDailyEntity) {
+                scoreDailyEntity = new ScoreDaily(scoreDailyId);
+              }
+
+              scoreDailyEntity.period = period;
+              if(entity.winner == 0) {
+                scoreDailyEntity.numWins++;
+              }
+              else {
+                scoreDailyEntity.numLosses++;
+              }
+
+              scoreDailyEntity.nearKart = entity.homeTokenId;
+              scoreDailyEntity.save();
+
+              let scoreMonthlyId = entity.homeTokenId + '_' + periodMonth.toString();
+              let scoreMonthlyEntity = ScoreMonthly.load(scoreMonthlyId);
+
+              if(!scoreMonthlyEntity) {
+                scoreMonthlyEntity = new ScoreMonthly(scoreMonthlyId);
+              }
+
+              scoreMonthlyEntity.period = periodMonth;
+              if(entity.winner == 0) {
+                scoreMonthlyEntity.numWins++;
+              }
+              else {
+                scoreMonthlyEntity.numLosses++;
+              }
+
+              scoreMonthlyEntity.nearKart = entity.homeTokenId;
+              scoreMonthlyEntity.save();
             }
-          }
-        }
-      }
-      else if(eventStr == 'game_simple_battle') {
-        let data = jo.get('data');
-        let dataObj = data ? data.toObject() : null;
-
-        if(dataObj) {
-          let entity = new NearKartsSimpleBattle(receiptWithOutcome.receipt.id.toHex())
-
-          entity.homeAccount = homeAccount;
-          entity.timestamp = timestamp;
-
-          let homeTokenId = dataObj.get('home_token_id');
-          entity.homeTokenId = homeTokenId ? homeTokenId.toString() : '';
-
-          let awayTokenId = dataObj.get('away_token_id');
-          entity.awayTokenId = awayTokenId ? awayTokenId.toString() : '';
-
-          let winner = dataObj.get('winner');
-          entity.winner = winner ? winner.toI64() as i32 : 0;
-          
-          let battle = dataObj.get('battle');
-          entity.battle = battle ? battle.toBigInt() : new BigInt(0);
-
-          let prize = dataObj.get('prize');
-          entity.prize = prize ? prize.toString() : '';
-
-          let extra = dataObj.get('extra');
-          entity.extra = extra ? extra.toString() : '';
-
-          entity.save();
-
-          let nearKart = NearKart.load(entity.homeTokenId);
-
-          if(nearKart) {
-            let scoreDailyId = entity.homeTokenId + '_' + period.toString();
-            let scoreDailyEntity = ScoreDaily.load(scoreDailyId);
-
-            if(!scoreDailyEntity) {
-              scoreDailyEntity = new ScoreDaily(scoreDailyId);
-            }
-
-            scoreDailyEntity.period = period;
-            if(entity.winner == 0) {
-              scoreDailyEntity.numWins++;
-            }
-            else {
-              scoreDailyEntity.numLosses++;
-            }
-
-            scoreDailyEntity.nearKart = entity.homeTokenId;
-            scoreDailyEntity.save();
-
-            let scoreMonthlyId = entity.homeTokenId + '_' + periodMonth.toString();
-            let scoreMonthlyEntity = ScoreMonthly.load(scoreMonthlyId);
-
-            if(!scoreMonthlyEntity) {
-              scoreMonthlyEntity = new ScoreMonthly(scoreMonthlyId);
-            }
-
-            scoreMonthlyEntity.period = periodMonth;
-            if(entity.winner == 0) {
-              scoreMonthlyEntity.numWins++;
-            }
-            else {
-              scoreMonthlyEntity.numLosses++;
-            }
-
-            scoreMonthlyEntity.nearKart = entity.homeTokenId;
-            scoreMonthlyEntity.save();
           }
         }
       }
     }
   }
+    
 
   // BigInt and BigDecimal math are supported
   // entity.count = entity.count + BigInt.fromI32(1)
