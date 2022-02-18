89cdc142fa4aecc0aceafcc8e086748e54e68731
Ben Razor <chrisbenwar1@googlemail.com>
2022-02-18 22:42:27 +0000

    Remove unused code

--- thegraph/nk/src/mapping.ts
+++ thegraph/nk/src/mapping.ts
@@ -1,12 +1,9 @@
-import { near, BigInt, JSONValue, json, log } from "@graphprotocol/graph-ts"
+import { near, BigInt, json, log } from "@graphprotocol/graph-ts"
 import { NearKartsSimpleBattle, ScoreDaily, ScoreMonthly, NearKart } from "../generated/schema"
 
 export function handleReceipt(
   receiptWithOutcome: near.ReceiptWithOutcome
 ): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  //let entity = NearKartsSimpleBattle.load(receiptWithOutcome.receipt.id.toHex())
 
   receiptWithOutcome.block.header
   let logs = receiptWithOutcome.outcome.logs;
@@ -151,19 +148,4 @@ export function handleReceipt(
       }
     }
   }
-    
-
-  // BigInt and BigDecimal math are supported
-  // entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on receipt information
-  // entity.block = receiptWithOutcome.block.header.hash
-
-  // Entities can be written to the store with `.save()`
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
 }
